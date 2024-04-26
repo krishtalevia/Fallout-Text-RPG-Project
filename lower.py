a68046159dc9e90b7e8e8e7e2b110410d36c5a2d
krishtalevia <krishtalevia@gmail.com>
2024-04-26 19:03:53 +0300

    dev character define

--- lower.py
+++ lower.py
@@ -7,8 +7,16 @@ def check_char_directory() -> None:
         print('test Папки содержащей профили не существовало, папка создана.')
 
 def character_deifne(char_status, char_name) -> None:
-    if char_status == 'new' and os.path.exists(rf'characters/{char_name}'):
+    if char_status == 'new' and os.path.exists(rf'characters/{char_name}') == True:
         answer = gui.char_exists()
 
         if answer == 'rewrite':
-            pass
+            return True
+        else:
+            return False
+
+    elif char_status == 'new' and os.path.exists(rf'characters/{char_name}') == False:
+        return True
+
+    elif char_status == 'load':
+        return True
