849176b39cc6069acc94e5c48104753bef6ed518
krishtalevia <krishtalevia@gmail.com>
2024-05-12 16:27:37 +0300

    dev gui

---
+++ gui_headers.py
@@ -0,0 +1,16 @@
+gr_color = '\033[0;32m'
+bl_color = '\033[0;34m'
+end_color = '\033[0m'
+
+def main_menu_header():
+    print(rf"""
+     ______      ____  {bl_color}/{end_color}         __ 
+    / ____/___ _/ / /_{bl_color}//{end_color}  __  __/ /_
+   / /_  / __ `/ / / {bl_color}//{end_color}\/ / / / __/
+  / __/ / /_/ / / / {bl_color}/-/{end_color} / /_/ / /_  
+ /_/    \____/_/_/\_{bl_color}//{end_color}_/\____/\__/  
+                   {bl_color}//{end_color}
+                   {bl_color}/{end_color}   
+--------------------------------------------------
+> Главное меню                  
+--------------------------------------------------""")
